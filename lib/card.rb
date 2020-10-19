0ec6ab03f14c6c9fb59e9140ebfb746aea72c0e2
Dan <daniel.bedford56@gmail.com>
2020-10-19 15:34:51 +0100

    Added touch in and touch out

--- lib/card.rb
+++ lib/card.rb
@@ -1,13 +1,44 @@
 class Oyster_Card
     attr_reader :balance
 
+    @@MAX_BALANCE = 90
+    @@MIN_FARE = 1
+
     def initialize()
         @balance = 0
+        @starting_station = nil
     end
 
     def top_up(monees)
-        @balance += monees
+        @balance + monees > @@MAX_BALANCE ? raise("This top up exeeds max balance of £90.") : @balance += monees
+        
         "Your balance is now £#{@balance}."
     end
+
+    def pay_fare(monees)
+        @balance -= monees
+        "You have paid £#{monees}."
+    end
+
+    def touch_in(station)
+        @balance >= @@MIN_FARE ? @starting_station = station : raise("You cannot cover minimum fare.")
+
+        "You have touched in at #{station}."
+    end
+
+    def touch_out(station)
+        deduct
+        @starting_station = nil
+        "You have touched out at #{station}."
+    end
+
+    def in_journey?
+        @starting_station != nil
+    end
+
+    private
+        def deduct
+            @balance -= @@MIN_FARE
+        end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,6 +12,55 @@ RSpec.describe Oyster_Card do
         it 'Allows the user to add money and returns new balance' do
             expect(subject.top_up(1)).to eq "Your balance is now £#{subject.balance}."
         end
+
+        it 'Raises an error if balance exeeds max (90)' do
+            expect{subject.top_up(91)}.to raise_error("This top up exeeds max balance of £90.")
+        end
+    end
+
+    describe 'pay_fare' do
+        it "Deducts the train fare from the user's balance" do
+            subject.top_up(1)
+            expect(subject.pay_fare(1)).to eq "You have paid £1."
+            expect(subject.balance).to eq 0
+        end
+    end
+
+    describe 'in_journey?' do
+        let(:station) {double("KX")}
+        it 'Returns true if in transit' do
+            subject.top_up(2)
+            subject.touch_in(station)
+            expect(subject.in_journey?).to eq true
+        end
+
+        it 'Returns false if not in transit' do
+            expect(subject.in_journey?).to eq false
+        end
+    end
+
+    describe 'touch_in' do
+        let(:station) {double("KX")}
+        it 'Sets the starting station attr and returns it' do
+            subject.top_up(2)
+            expect(subject.touch_in(station)).to eq "You have touched in at #{station}."
+        end
+
+        it 'Raises an error if user has not got minimum balance.' do
+            expect{subject.touch_in(station)}.to raise_error "You cannot cover minimum fare."
+        end
+    end
+
+    describe 'touch_out' do
+        let(:station) {double("Blackfriars")}
+        it 'Sets the finishing station' do
+            expect(subject.touch_out(station)).to eq "You have touched out at #{station}."
+        end
+
+        it 'Deducts min fare from balance' do
+            subject.top_up(2)
+            expect{subject.touch_out(station)}.to change{subject.balance}.by(-1)
+        end
     end
 
 end
